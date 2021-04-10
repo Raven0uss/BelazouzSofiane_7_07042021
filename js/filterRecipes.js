445f17c08e2d76e7745971c7fa24598fb90f8134
Sofiane BELAZOUZ <sofianebelazouz@gmail.com>
2021-04-10 12:59:55 +0200

    init branch version b

--- js/filterRecipes.js
+++ js/filterRecipes.js
@@ -1,8 +1,7 @@
 // This is the function to filter the list of recipes.
 // It returns a new list to render
 
-// Waiting pull request of one of each version of algorithm
-// Check out the branches search-algo-a and search-algo-b
+// VERSION B
 const filterRecipes = ({ recipes, tags, search }) => {
   let result = [];
 
